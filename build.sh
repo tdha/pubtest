441cbc82a27d7901b529b582604e3a53fb155be2
Ton Ha <ton@tdha.co>
2024-04-18 15:36:38 +1000

    build.sh

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ python3 manage.py collectstatic --no-input
 # Apply any outstanding database migrations
 python3 manage.py migrate
 
-if [[ $CREATE_SUPERUSER ]]
-then
-    python3 manage.py createsuperuser --no-input
-fi 
+# if [[ $CREATE_SUPERUSER ]]
+# then
+#     python3 manage.py createsuperuser --no-input
+# fi 
